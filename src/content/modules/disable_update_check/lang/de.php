72511737894a8216319befc500eded0cc03e2219
Ulrich Schmidt <u.schmidt@velian.de>
2016-11-21 16:08:01 +0100

    sprachdatei

---
+++ src/content/modules/disable_update_check/lang/de.php
@@ -0,0 +1,6 @@
+<?php
+define ( "TRANSLATION_DISABLE_UPDATE_CHECK", "Prüfung auf Updates" );
+define ( "TRANSLATION_DISABLE_PACKAGE_UPDATE_CHECK", "Prüfung auf Aktualisierungen für Pakete deaktivieren" );
+define ( "TRANSLATION_DISABLE_CORE_UPDATE_CHECK", "Prüfung auf neue UliCMS Releases deaktivieren" );
+define ( "TRANSLATION_DISABLE_CORE_PATCH_CHECK", "Prüfung auf neue Patches deaktivieren" );
+define ( "TRANSLATION_DISABLE_ULICMS_NEWSFEED", "Deaktiviere die Anzeige des ulicms.de Newsfeed" );
---
+++ src/content/modules/disable_update_check/lang/en.php
@@ -0,0 +1,6 @@
+<?php
+define ( "TRANSLATION_DISABLE_UPDATE_CHECK", "Check for Updates" );
+define ( "TRANSLATION_DISABLE_PACKAGE_UPDATE_CHECK", "Disable check for package updates" );
+define ( "TRANSLATION_DISABLE_CORE_UPDATE_CHECK", "Disable check for new UliCMS Releases" );
+define ( "TRANSLATION_DISABLE_CORE_PATCH_CHECK", "Disable check for Patches" );
+define ( "TRANSLATION_DISABLE_ULICMS_NEWSFEED", "Disable display of ulicms.de Newsfeed" );
